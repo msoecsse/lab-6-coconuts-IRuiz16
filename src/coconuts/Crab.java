5675c43201fdcc5aafc69067add361f060654bf5
Trevor-Hancock5 <hancockt@msoe.edu>
2025-10-14 09:49:18 -0500

    Objects hit each other

--- src/coconuts/Crab.java
+++ src/coconuts/Crab.java
@@ -5,7 +5,7 @@ import javafx.scene.image.Image;
 // Represents the object that shoots down coconuts but can be hit by coconuts. Killing the
 //   crab ends the game
 // This is a domain class; other than Image, do not introduce JavaFX or other GUI components here
-public class Crab extends HittableIslandObject {
+public class Crab extends IslandObject {
     private static final int WIDTH = 50; // assumption: height and width are the same
     private static final Image crabImage = new Image("file:images/crab-1.png");
 
@@ -29,12 +29,5 @@ public class Crab extends HittableIslandObject {
             x = xMin;
         }
         display();
-    }
-
-    @Override
-    public void onHit(IslandObject source, HittableIslandObject obj) {
-        if(obj == this){
-            //TODO
-        }
     }
 }
